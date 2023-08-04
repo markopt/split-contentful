12fb8e2d011608edf8fa6b33d8b31c4586255d33
ColeHuntley83 <cole@trackonomysystems.com>
2023-08-04 16:42:12 -0700

    apis working

--- src/components/SplitExperiments.jsx
+++ src/components/SplitExperiments.jsx
@@ -3,44 +3,36 @@ import { Paragraph } from "@contentful/f36-components";
 import { /* useCMA, */ useSDK } from "@contentful/react-apps-toolkit";
 import axios from "axios";
 import useSWR from "swr";
-import SplitExperiments from "../components/SplitExperiments";
-const Field = () => {
-  const [splitExp, setSplitExp] = useState([]);
-const sdk = useSDK();
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-     const baseURL = 'https://api.split.io/internal/api/v2/splits';
-     
-  */
- 
+
+const SplitExperiments = ({experimnent}) => {
+  const [splitDefs, setSplitDefs] = useState([]);
  
  const workspaceId = "ff211590-acc0-11ed-b267-ee0a963b46a9";
  const baseURL = "https://api.split.io/internal/api/v2/splits";
  const getDefinitionUrl = `/ws/${workspaceId}/asb_flag/environments/Local-Default`
   const fetcher = (url) => axios.get(`${baseURL}${url}`, { headers: {
       Authorization: `Bearer kvrf8dnvhqo462phrngs9ke2l0rj7aet79vl`,
-    }}).then((res) => res.data?.objects);
+    }}).then((res) => res.data);
   const { data, error, isLoading, isFetching } = useSWR(
-    `/ws/${workspaceId}?tag=experiment`,
+    `/ws/ff211590-acc0-11ed-b267-ee0a963b46a9/${experimnent}/environments/04d48470-acc3-11ed-8db0-02f989b7d254`,
     fetcher
   );  
-  console.log("data: ", data)
-  console.log("error: ", error)
-  console.log("isloading: ", isLoading)
+  console.log("exp data: ", data)
+  console.log("exp error: ", error)
+  console.log("exp isloading: ", isLoading)
 
-useEffect(() => {
-  if (data && data.length && data[0]?.name) { 
-      setSplitExp(data)
-  }
+// useEffect(() => {
+//   if (data && data.length && data[0]?.name) { 
+//       setSplitExp(data)
+//   }
 
-}, [data])
+// }, [data])
   const CONTENT_FIELD_ID = "cta";
 
   
 
   //const contentField = sdk.entry.fields[CONTENT_FIELD_ID];
-   sdk.field.setValue("hello world");
+  // sdk.field.setValue("hello world");
   // console.log(contentField);
   // console.log("hello world from entry editor");
   // const cma = useCMA();
@@ -50,22 +42,17 @@ useEffect(() => {
   console.log("data here", data)
   return (
     <>
-    {/* {splitExp && splitExp.length && splitExp.map((obj, i) => {
-      return (
-      <div key={i}>
-        {obj.name}
-      </div>
-      )
-    })} */}
-<Paragraph>{splitExp && splitExp?.[0]?.name}</Paragraph>
 
-    <SplitExperiments
-    experimnent={splitExp && splitExp?.[0]?.name}
-    
-    />
-    
+
+    {data && data.treatments?.length &&data.treatments.map((obj, i) => {
+        return (
+            <Paragraph>
+                {`[${obj.name}, ${obj.description}]`}
+            </Paragraph>
+        )
+    }) }
     </>
   );
 };
 
-export default Field;
+export default SplitExperiments;
